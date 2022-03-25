bab220b17887fca757efd1660dc992e6e899f5af
AkenEagle <akeneagle@gmail.com>
2022-03-25 15:05:20 +0000

    my-work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { Banner } from "./components/Banner";
 import { AboutMe } from "./components/AboutMe";
+import { MyWork } from "./components/MyWork";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Banner />
       <AboutMe />
+      <MyWork />
     </main>
   );
 }
---
+++ src/components/MyWork.js
@@ -0,0 +1,296 @@
+import React from "react";
+
+import "../App.css";
+
+export const MyWork = () => {
+  return (
+    <section id="my-work" className="content">
+      <div className="content-title">
+        <h3>My work</h3>
+      </div>
+      <div className="content-box">
+        <div className="card" id="main-project">
+          <img src="./assets/images/readers-revival.png" />
+          <div className="card-info">
+            <p>Readers Revival</p>
+            <ul>
+              <li>
+                <a
+                  href="https://asmaamusse.github.io/readers-revival/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                  View Live
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AsmaaMusse/readers-revival"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                  View Project
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/employee-tracker.png" />
+          <div className="card-info">
+            <p>Employee Tracker</p>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/employee-tracker"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/employee-tracker"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/readme-generator.jpg" />
+          <div className="card-info">
+            <p>Readme Generator</p>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/readme-generator"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/readme-generator"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/text-editor.png" />
+          <div className="card-info">
+            <p>Text Editor</p>
+            <ul>
+              <li>
+                <a
+                  href="https://young-ocean-60222.herokuapp.com/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/text-editor"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/food4u.png" />
+          <div className="card-info">
+            <p>Food 4 U</p>
+            <ul>
+              <li>
+                <a href="https://food-4u.herokuapp.com/" target="_blank">
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a href="https://github.com/AkenEagle/food4U" target="_blank">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/note-taker.png" />
+          <div className="card-info">
+            <p>Note Taker</p>
+            <ul>
+              <li>
+                <a
+                  href="https://note-taker-harman.herokuapp.com/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/note-taker"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/tech-blog.png" />
+          <div className="card-info">
+            <p>Tech Blog</p>
+            <ul>
+              <li>
+                <a
+                  href="https://tech-blog-handlebars.herokuapp.com/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/tech-blog"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/team-profile-generator.png" />
+          <div className="card-info">
+            <p>Team Profile Generator</p>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/team-profile-generator"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/team-profile-generator"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/work-day-scheduler.png" />
+          <div className="card-info">
+            <p>Work Day Scheduler</p>
+            <ul>
+              <li>
+                <a
+                  href="https://akeneagle.github.io/work-day-scheduler/index.html"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/work-day-scheduler"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/password-generator.png" />
+          <div className="card-info">
+            <p>Password Generator</p>
+            <ul>
+              <li>
+                <a
+                  href="https://akeneagle.github.io/password-generator/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/password-generator"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./assets/images/code-quiz.png" />
+          <div className="card-info">
+            <p>Code Quiz</p>
+            <ul>
+              <li>
+                <a
+                  href="https://akeneagle.github.io/code-quiz/"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-rocket"></i>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/AkenEagle/code-quiz"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
