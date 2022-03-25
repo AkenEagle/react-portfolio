6b94e8491aa3b9c43651a75b379217e0894fcc12
AkenEagle <akeneagle@gmail.com>
2022-03-25 14:47:30 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import { createTheme, ThemeProvider } from "@material-ui/core";
+import { Navbar } from "./components/Navbar";
 import "./App.css";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ff0000",
-    },
-    secondary: {
-      main: "#00ff00",
-    },
-  },
-  typography: {
-    fontFamily: "Poppins",
-  },
-});
-
 function App() {
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme} />
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "../App.css";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <h1>
+        <a href="./">Harmanjot Singh</a>
+      </h1>
+
+      <nav class="navbar">
+        <ul>
+          <li>
+            <a href="#about-me">About me</a>
+          </li>
+          <li>
+            <a href="#my-work">My work</a>
+          </li>
+          <li>
+            <a href="#contact-me">Contact me</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
